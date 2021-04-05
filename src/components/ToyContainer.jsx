cb4ffbd3ad9c712bb9b2d575c81293c0b9aa348e
Alicia Weenum <weenum.alicia@gmail.com>
2021-04-05 19:11:15 -0400

    commit tree cf12825b89793f056457cc6b20d05a9cc53d4a3b

--- src/components/ToyContainer.jsx
+++ src/components/ToyContainer.jsx
@@ -4,6 +4,7 @@ import ToyCard from './ToyCard'
 const ToyContainer = () => {
   return(
     <div id="toy-collection">
+    <ToyCard/>
       {/* Render the collection of ToyCards */}
     </div>
   );
